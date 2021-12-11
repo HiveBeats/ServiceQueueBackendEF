130cf1d427e33ed457515a57fafd6619e5640a96
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2021-12-11 18:51:52 +0700

    wip: userFiller

---
+++ UserFiller/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace UserFiller
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+        
+        public static async Task Main(string[] args)
+        {
+            //todo: setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+            
+            //configuration
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true,true)
+                .AddEnvironmentVariables()
+                .Build();
+            
+            //api setup
+            var apiBase = Configuration.GetSection("Connection:WebApi").Value;
+            var user = new User();
+            Configuration.GetSection(nameof(User)).Bind(user);
+            
+            //action
+            using (var client = new HttpClient())
+            {
+                var result = await client.PostAsJsonAsync($"{apiBase}/Auth/Register", user);
+                if (result.IsSuccessStatusCode)
+                {
+                    //todo: logging
+                }
+            }
+        }
+    }
+}
