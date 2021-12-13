f83df5725bfe46150e8faba67b65244725e9bdea
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2021-12-13 21:46:17 +0700

    feature: logging

--- UserFiller/Program.cs
+++ UserFiller/Program.cs
@@ -16,29 +16,43 @@ namespace UserFiller
         public static async Task Main(string[] args)
         {
             //todo: setup our DI
-            var serviceProvider = new ServiceCollection()
-                .AddLogging()
-                .BuildServiceProvider();
-            
-            //configuration
-            Configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", true, true)
-                .AddEnvironmentVariables(prefix: "FILLR")
-                .Build();
-            
-            //api setup
-            var apiBase = Configuration.GetSection("Connection:WebApi").Value;
-            var user = new User();
-            Configuration.GetSection(nameof(User)).Bind(user);
-            
-            //action
-            using (var client = new HttpClient())
+            using (var serviceProvider = new ServiceCollection()
+                .AddLogging(config => config.ClearProviders().AddConsole().SetMinimumLevel(LogLevel.Trace))
+                .BuildServiceProvider())
             {
-                var result = await client.PostAsJsonAsync($"{apiBase}/Auth/Register", user);
-                if (result.IsSuccessStatusCode)
+                //configure console logging
+                serviceProvider.GetService<ILoggerFactory>();
+                
+                var logger = serviceProvider.GetService<ILoggerFactory>()
+                    .CreateLogger<Program>();
+                
+                //configuration
+                Configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddEnvironmentVariables(prefix: "FILLR")
+                    .Build();
+            
+                //api setup
+                var apiBase = Configuration.GetSection("Connection:WebApi").Value;
+                var user = new User();
+                Configuration.GetSection(nameof(User)).Bind(user);
+                
+                logger.LogInformation($"FILLR starts with: {apiBase}");
+            
+                //action
+                using (var client = new HttpClient())
                 {
-                    //todo: logging
+                    logger.LogInformation("FILLR requesting...");
+                    
+                    var result = await client.PostAsJsonAsync($"{apiBase}/Auth/Register", user);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        logger.LogInformation("FILLR request unsuccessfull");
+                    }
+                    else logger.LogInformation("FILLR request successfull");
                 }
+                
+                logger.LogInformation("FILLR closing.");
             }
         }
     }
