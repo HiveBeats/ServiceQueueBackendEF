0094991091ddac65ae2a286122a91bedb84ded25
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-01-06 22:45:12 +0700

    fix: old compose version

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 #check for env
 filename=.env
-if [ -f "$filename" ];
-then
-#stop old compose
-if echo "Stopping docker" && docker-compose down; then
-    #update version
-    if echo "Updating packages" && sh ./update-full.sh; then
-        #build frontend
-        if echo "Building frontend" && cd ./Frontend && sh ./build-prod.sh && cd ../; then
-            #up compose
-            echo "Running docker" && docker-compose up -d
+if [ -f "$filename" ]; then
+    if ! type "docker-compose -h" > /dev/null; then
+        echo "new version compose not found, trying the old one";
+        if echo "Stopping docker" && docker compose down; then
+            #update version
+            if echo "Updating packages" && sh ./update-full.sh; then
+                #build frontend
+                if echo "Building frontend" && cd ./Frontend && sh ./build-prod.sh && cd ../; then
+                    #up compose
+                    echo "Running docker" && docker compose up -d
+                fi
+            fi
+        fi
+    else 
+        #stop old compose
+        if echo "Stopping docker" && docker-compose down; then
+            #update version
+            if echo "Updating packages" && sh ./update-full.sh; then
+                #build frontend
+                if echo "Building frontend" && cd ./Frontend && sh ./build-prod.sh && cd ../; then
+                    #up compose
+                    echo "Running docker" && docker-compose up -d
+                fi
+            fi
         fi
     fi
-fi
 else
-echo "env file has not been found"
+    echo "env file has not been found"
 fi
