7f98434f0857cff35fc532d36b9ca2424a4a6a9b
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-01-29 06:31:44 +0700

    Feat deploy scripts (#14)

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#check for env
+filename=.env
+if [ -f "$filename" ]; then
+    denver -t ./sqlinit/01-databases.sql
+    if ! type "docker-compose -h" > /dev/null; then
+        echo "new version compose not found, trying the old one";
+        if echo "Stopping docker" && docker compose down; then
+            #update version
+            if echo "Updating packages" && sh ./update-full.sh; then
+                #build frontend
+                if echo "Building frontend" && cd ./Frontend && sh ./build-prod.sh && cd ../; then
+                    #up compose
+                    echo "Running docker" && docker compose up -d
+                fi
+            fi
+        fi
+    else
+        #stop old compose
+        if echo "Stopping docker" && docker-compose down; then
+            #update version
+            if echo "Updating packages" && sh ./update-full.sh; then
+                #build frontend
+                if echo "Building frontend" && cd ./Frontend && sh ./build-prod.sh && cd ../; then
+                    #up compose
+                    echo "Running docker" && docker-compose up -d
+                fi
+            fi
+        fi
+    fi
+else
+    echo "env file has not been found"
+fi
