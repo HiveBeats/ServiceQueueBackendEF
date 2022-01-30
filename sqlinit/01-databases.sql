b8cd3a213f096163756172771f267a41286b692c
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-01-30 20:39:45 +0700

    fix[mysql]: create user (#18)
    * fix: host usage for root
    
    * fix: initialer usage

--- sqlinit/01-databases.sql
+++ sqlinit/01-databases.sql
@@ -1,11 +1,11 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
+-- # create databases
+-- CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
 
-# create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+-- # create root user and grant rights
+-- CREATE USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 
-CREATE USER 'john'@'localhost' IDENTIFIED BY '${MYSQL_JOHN_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO 'john'@'localhost' WITH GRANT OPTION;
-CREATE USER 'john'@'%' IDENTIFIED BY '${MYSQL_JOHN_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO 'john'@'%' WITH GRANT OPTION;
+-- CREATE USER 'john'@'localhost' IDENTIFIED BY '${MYSQL_JOHN_PASSWORD}';
+-- GRANT ALL PRIVILEGES ON *.* TO 'john'@'localhost' WITH GRANT OPTION;
+-- CREATE USER 'john'@'%' IDENTIFIED BY '${MYSQL_JOHN_PASSWORD}';
+-- GRANT ALL PRIVILEGES ON *.* TO 'john'@'%' WITH GRANT OPTION;
