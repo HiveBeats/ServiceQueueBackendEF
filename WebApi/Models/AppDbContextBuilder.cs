2ecaa2d43aa255743ad17a4e227a5b20f9be62de
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-02-05 22:45:36 +0700

    feat[script]: entity indexes

--- WebApi/Models/AppDbContextBuilder.cs
+++ WebApi/Models/AppDbContextBuilder.cs
@@ -19,6 +19,12 @@ namespace WebApi.Models
             modelBuilder.Entity<Topic>().HasIndex(t => t.Name).IsUnique();
 
             modelBuilder.Entity<Message>().HasIndex(m => m.DateSolved);
+
+            modelBuilder.Entity<Script>().HasIndex(s => s.Name).IsUnique();
+            modelBuilder.Entity<Script>().HasIndex(s => s.Priority);
+            modelBuilder.Entity<Script>().HasIndex(s => s.IsEnabled);
+
+            modelBuilder.Entity<ScriptLog>().HasIndex(sl => sl.LogLevel);
         }
 
         public static void ConfigureIdentityModels(ModelBuilder builder)
